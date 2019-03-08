0fdca83d31d2c77b8893c2c8b2e83883bebd4abc
Zlatko Rusev <zlatko.rusev@students.softuni.bg>
2019-03-08 23:45:50 +0200

    Home component

---
+++ flight_app/src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class HomePage extends Component {
+    render() {
+
+        return (
+            'Home Page'
+        )
+    }
+}
+
+export default HomePage;
