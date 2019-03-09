3ff34580e414ffc954d8e490fa614f11ce4e8c00
Zlatko Rusev <zlatko.rusev@students.softuni.bg>
2019-03-09 20:44:08 +0200

    NavBar component

--- flight_app/src/App.jsx
+++ flight_app/src/App.jsx
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom'
-import HomePage from './components/HomePage';
+import NavBar from './components/common/NavBar/NavBarLayout';
+import HomePage from './components/common/HomePage/HomePage';
 import NotFoundPage from './components/common/NotFound/NotFoundPage';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
+        <header>
+          <link rel="stylesheet" 
+                href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+                integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" 
+                crossOrigin="anonymous"
+          />
+          <NavBar />
+        </header>
         <main>
           <Switch>
             <Route exact path='/' component={HomePage} />
--- flight_app/src/index.js
+++ flight_app/src/index.js
@@ -4,7 +4,6 @@ import './style/App.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom'
 
-
 ReactDOM.render(
 <Router>
     <App />
